a1bed3245a5cc026802027eef62ef21f4443373f
sherifmorshedd-2 <sherif.morshedd@gmail.com>
2026-07-09 16:49:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  requests are NEVER cached to prevent stale data.
 // ══════════════════════════════════════════
 
-const CACHE_NAME = 'land-wells-v65';
+const CACHE_NAME = 'land-wells-v68';
 
 // Only cache static files that don't change between sessions
 const STATIC_ASSETS = [
